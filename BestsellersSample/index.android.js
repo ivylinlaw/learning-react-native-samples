54f010c7d3085dc978f537bfcf3d8ec48e45d05e
vyiii <ivylinlaw@Ivys-MacBook-Pro-2.local>
2016-06-10 01:15:48 -0700

    commit chp.4 samples

---
+++ BestsellersSample/index.android.js
@@ -0,0 +1,4 @@
+var React = require('react-native');
+var { AppRegistry } = React;
+var BestsellersSample = require('./BookList');
+AppRegistry.registerComponent('BestsellersSample', () => BestsellersSample);
--- TouchSample/index.ios.js
+++ TouchSample/index.ios.js
@@ -1,4 +1,6 @@
 var React = require('react-native');
 var { AppRegistry } = React;
-var PressDemo = require('./PressDemo');
-AppRegistry.registerComponent('TouchSample', () => PressDemo);
+// var PressDemo = require('./PressDemo');
+// AppRegistry.registerComponent('TouchSample', () => PressDemo);
+var PanDemo = require('./PanDemo');
+AppRegistry.registerComponent('TouchSample', () => PanDemo);
