d8a89582f583a7bc54cf7a63c5925b76f4ad6842
vyiii <ivylinlaw@Ivys-MacBook-Pro-2.local>
2016-06-16 10:32:26 -0700

    combine all projects

---
+++ BestsellersSample/BookList.js
@@ -0,0 +1,116 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ListView,
+} from 'react-native';
+
+var BookItem = require('./BookItem');
+var API_KEY = '73b19491b83909c7e07016f4bb4644f9:2:60667290';
+var QUERY_TYPE = 'hardcover-fiction';
+var API_STEM = 'http://api.nytimes.com/svc/books/v3/lists';
+//var ENDPOINT = '${API_STEM}/${QUERY_TYPE}?response-format=json&api-key=${API_KEY}';
+var ENDPOINT = API_STEM+'/'+QUERY_TYPE+'?response-format=json&api-key='+API_KEY;
+
+var BookList = React.createClass({
+	getInitialState: function() {
+		var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		return {
+			dataSource: ds.cloneWithRows([])
+		};
+	},
+
+	componentDidMount: function() {
+		this._refreshData();
+	},
+
+	_renderRow: function(rowData) {
+		return <BookItem coverURL={rowData.book_image}
+		title={rowData.title}
+		author={rowData.author}/>;
+	},
+
+	_renderHeader: function() {
+		return (<View style={styles.sectionDivider}>
+			<Text style={styles.headingText}>
+				Bestsellers in Hardcover Fiction
+			</Text>
+			</View>);
+	},
+
+	_renderFooter: function() {
+		return(
+			<View style={styles.sectionDivider}>
+				<Text>Data from the New York Times Best Seller list.</Text>
+			</View>
+			);
+	},
+
+	_refreshData: function() {
+		fetch(ENDPOINT)
+			.then((response) => response.json())
+			.then((rjson) => {
+				this.setState({
+					dataSource: this.state.dataSource.cloneWithRows(rjson.results.books)
+				});
+			});
+	},
+
+	render: function() {
+		return (
+			<ListView
+				//style=
+				dataSource={this.state.dataSource}
+				renderRow={this._renderRow}
+				renderHeader={this._renderHeader}
+				renderFooter={this._renderFooter}/>
+		);
+	},
+});
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#FFFFFF',
+		paddingTop: 24
+	},
+
+	list: {
+		flex: 1,
+		flexDirection: 'row'
+	},
+
+	listContent: {
+		flex: 1,
+		flexDirection: 'column'
+	},
+
+	row: {
+		flex: 1,
+		fontSize: 18,
+		padding: 38,
+		borderWidth: 1,
+		borderColor: '#DDDDDD'
+	},
+
+	sectionDivider: {
+		padding: 8,
+		paddingTop: 24,
+		backgroundColor: '#EEEEEE',
+		alignItems: 'center'
+	},
+
+	headingText: {
+		flex: 1,
+		fontSize: 24,
+		alignSelf: 'center'
+	},
+});
+
+module.exports = BookList;
---
+++ StyleSample/index.ios.js
@@ -0,0 +1,4 @@
+var React = require('react-native');
+var { AppRegistry } = React;
+var StyleSample = require('./main');
+AppRegistry.registerComponent('StyleSample', () => StyleSample);
